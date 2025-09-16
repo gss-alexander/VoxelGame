08b484c7ac644301c91eef0a1280f04c2246e0e9
Alexander Groth <alexander@grothsoftwaresolutions.no>
2025-09-16 22:36:28 +0200

    Add empty panel component

---
+++ Client/UiSystem/Components/Panel.cs
@@ -0,0 +1,22 @@
+using System.Numerics;
+
+namespace Client.UiSystem.Components;
+
+public class Panel : UiElement
+{
+    public override void Update(float deltaTime)
+    {
+    }
+
+    public override void Render(float deltaTime)
+    {
+        foreach (var child in Children)
+        {
+            child.Render(deltaTime);
+        }
+    }
+
+    public override void HandleInput(Vector2 mousePosition, bool isClicked)
+    {
+    }
+}
